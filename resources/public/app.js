79a8022b1dfe98b90918f2d638f32c747f96ca9b
Ske <voltasalt@gmail.com>
2017-04-03 22:53:49 +0200

    Initial commit

---
+++ resources/public/app.js
@@ -0,0 +1,310 @@
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) === variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+}
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+window.App = {
+    elements: {
+        board: $("#board"),
+        palette: $(".palette"),
+        boardMover: $(".board-mover"),
+        boardZoomer: $(".board-zoomer"),
+        boardContainer: $(".board-container"),
+        cursor: $(".cursor"),
+        timer: $(".cooldown-timer"),
+        reticule: $(".reticule"),
+        alert: $(".message"),
+        coords: $(".coords")
+    },
+    panX: 0,
+    panY: 0,
+    scale: 4,
+    cooldown: 0,
+    init: function () {
+        this.color = -1;
+
+        $(".board-container").hide();
+        $(".reticule").hide();
+        $(".ui").hide();
+        $(".message").hide();
+        $(".cursor").hide();
+        $(".cooldown-timer").hide();
+
+        $.get("/boardinfo", this.initBoard.bind(this));
+
+        this.initBoardMovement();
+        this.initBoardPlacement();
+        this.initCursor();
+        this.initReticule();
+        this.initAlert();
+        this.initCoords();
+        Notification.requestPermission();
+    },
+    initBoard: function (data) {
+        this.width = data.width;
+        this.height = data.height;
+        this.palette = data.palette;
+
+        this.initPalette();
+
+        this.elements.board.attr("width", this.width).attr("height", this.height);
+
+        this.updateTransform();
+
+        var cx = getQueryVariable("x") || this.width / 2;
+        var cy = getQueryVariable("y") || this.height / 2;
+        this.centerOn(cx, cy);
+
+        this.scale = getQueryVariable("scale") || this.scale;
+        this.updateTransform();
+
+        this.initSocket();
+        setInterval(this.updateTime.bind(this), 1000);
+        jQuery.get("/boarddata", this.drawBoard.bind(this));
+    },
+    drawBoard: function (data) {
+        var ctx = this.elements.board[0].getContext("2d");
+
+        var id = new ImageData(this.width, this.height);
+        var intView = new Uint32Array(id.data.buffer);
+
+        var rgbPalette = this.palette.map(function (c) {
+            var rgb = hexToRgb(c);
+            return 0xff000000 | rgb.b << 16 | rgb.g << 8 | rgb.r;
+        });
+
+        for (var i = 0; i < this.width * this.height; i++) {
+            intView[i] = rgbPalette[data.charCodeAt(i)];
+        }
+
+        ctx.putImageData(id, 0, 0);
+    },
+    initPalette: function () {
+        this.palette.forEach(function (color, idx) {
+            $("<div>")
+                .addClass("palette-color")
+                .css("background-color", color)
+                .click(function () {
+                    if (this.cooldown === 0) {
+                        this.switchColor(idx);
+                    } else {
+                        this.switchColor(-1);
+                    }
+                }.bind(this))
+                .appendTo(this.elements.palette);
+        }.bind(this));
+    },
+    initBoardMovement: function () {
+        var dragX = 0;
+        var dragY = 0;
+        var down = false;
+
+        this.elements.boardContainer.on("mousedown", function (evt) {
+            dragX = evt.screenX;
+            dragY = evt.screenY;
+            down = true;
+        }.bind(this)).on("mousemove", function (evt) {
+            if (!down) return;
+            var dx = evt.screenX - dragX,
+                dy = evt.screenY - dragY;
+            this.panX += dx / this.scale;
+            this.panY += dy / this.scale;
+            dragX = evt.screenX;
+            dragY = evt.screenY;
+
+            this.updateTransform()
+        }.bind(this)).on("mouseup", function (evt) {
+            down = false;
+        }.bind(this)).on("mouseout", function (evt) {
+            down = false;
+        }.bind(this)).on("wheel", function (evt) {
+            var oldScale = this.scale;
+
+            if (evt.originalEvent.deltaY > 0) {
+                this.scale /= 2
+            } else {
+                this.scale *= 2;
+            }
+
+            this.scale = Math.min(40, Math.max(2, this.scale));
+
+            var dx = evt.clientX - this.elements.boardContainer.width() / 2;
+            var dy = evt.clientY - this.elements.boardContainer.height() / 2;
+
+            this.panX -= dx / oldScale;
+            this.panX += dx / this.scale;
+
+            this.panY -= dy / oldScale;
+            this.panY += dy / this.scale;
+
+            this.updateTransform();
+        }.bind(this));
+    },
+    initBoardPlacement: function () {
+        var downX, downY;
+
+        this.elements.board.on("mousedown", function (evt) {
+            downX = evt.clientX;
+            downY = evt.clientY;
+        }).on("click", function (evt) {
+            if (downX === evt.clientX && downY === evt.clientY && this.color !== -1 && this.cooldown === 0) {
+                var pos = this.screenToBoardSpace(evt.clientX, evt.clientY);
+                this.place(pos.x, pos.y);
+            }
+        }.bind(this)).contextmenu(function (evt) {
+            evt.preventDefault();
+            this.switchColor(-1);
+        }.bind(this));
+    },
+    initCursor: function () {
+        $(document.body).on("mousemove", function (evt) {
+            this.elements.cursor.css("transform", "translate(" + evt.clientX + "px, " + evt.clientY + "px)");
+        }.bind(this));
+    },
+    initReticule: function () {
+        this.elements.board.on("mousemove", function (evt) {
+            var boardPos = this.screenToBoardSpace(evt.clientX, evt.clientY);
+            boardPos.x |= 0;
+            boardPos.y |= 0;
+
+            var screenPos = this.boardToScreenSpace(boardPos.x, boardPos.y);
+            this.elements.reticule.css("transform", "translate(" + screenPos.x + "px, " + screenPos.y + "px)");
+            this.elements.reticule.css("width", this.scale + "px").css("height", this.scale + "px");
+
+            if (this.color === -1) {
+                this.elements.reticule.hide();
+            } else {
+                this.elements.reticule.show();
+            }
+        }.bind(this));
+    },
+    initCoords: function () {
+        this.elements.board.on("mousemove", function (evt) {
+            var boardPos = this.screenToBoardSpace(evt.clientX, evt.clientY);
+
+            this.elements.coords.text("(" + boardPos.x + ", " + boardPos.y + ")");
+        }.bind(this));
+    },
+    initAlert: function () {
+        this.elements.alert.find(".close").click(function () {
+            this.elements.alert.fadeOut(200);
+        }.bind(this));
+    },
+    initSocket: function () {
+        var l = window.location;
+        var url = ( (l.protocol === "https:") ? "wss://" : "ws://") + l.host + l.pathname + "ws";
+
+        var ws = new WebSocket(url);
+        ws.onmessage = function (msg) {
+            var data = JSON.parse(msg.data);
+
+            if (data.type === "pixel") {
+                var ctx = this.elements.board[0].getContext("2d");
+                ctx.fillStyle = this.palette[data.color];
+                ctx.fillRect(data.x, data.y, 1, 1);
+            } else if (data.type === "alert") {
+                this.alert(data.message);
+            } else if (data.type === "cooldown") {
+                this.cooldown = Math.ceil(data.wait);
+                this.updateTime();
+            }
+        }.bind(this);
+
+        $(".board-container").show();
+        $(".ui").show();
+        $(".loading").fadeOut(500);
+
+        this.socket = ws;
+    },
+    updateTransform: function () {
+        this.elements.boardMover
+            .css("width", this.width + "px")
+            .css("height", this.height + "px")
+            .css("transform", "translate(" + this.panX + "px, " + this.panY + "px)");
+        this.elements.boardZoomer.css("transform", "scale(" + this.scale + ")");
+    },
+    screenToBoardSpace: function (screenX, screenY) {
+        var boardBox = this.elements.board[0].getBoundingClientRect();
+        var boardX = (((screenX - boardBox.left) / this.scale) | 0),
+            boardY = (((screenY - boardBox.top) / this.scale) | 0);
+        return {x: boardX, y: boardY};
+    },
+    boardToScreenSpace: function (boardX, boardY) {
+        var boardBox = this.elements.board[0].getBoundingClientRect();
+        var x = boardX * this.scale + boardBox.left,
+            y = boardY * this.scale + boardBox.top;
+        return {x: x, y: y};
+    },
+    centerOn: function (x, y) {
+        this.panX = (500 - x) - 0.5;
+        this.panY = (500 - y) - 0.5;
+        this.updateTransform();
+    },
+    switchColor: function (newColor) {
+        this.color = newColor;
+
+        if (newColor === -1) {
+            this.elements.cursor.hide();
+        } else {
+            this.elements.cursor.show();
+            this.elements.cursor.css("background-color", this.palette[newColor]);
+        }
+    },
+    place: function (x, y) {
+        this.socket.send(JSON.stringify({
+            x: x,
+            y: y,
+            color: this.color
+        }));
+        this.switchColor(-1);
+    },
+    alert: function (message) {
+        var alert = this.elements.alert;
+        alert.find(".text").text(message);
+        alert.fadeIn(200);
+    },
+    updateTime: function () {
+        var last = this.cooldown;
+
+        this.cooldown -= 1;
+        if (this.cooldown < 0) this.cooldown = 0;
+        this.cooldown |= 0;
+
+        if (this.cooldown !== 0) {
+            this.elements.timer.show();
+            var secs = Math.floor(this.cooldown % 60);
+            var secsStr = secs < 10 ? "0" + secs : secs;
+            var minutes = Math.floor(this.cooldown / 60);
+            var minuteStr = minutes < 10 ? "0" + minutes : minutes;
+            this.elements.timer.text(minuteStr + ":" + secsStr);
+
+            $(".palette-color").css("cursor", "not-allowed")
+        } else {
+            this.elements.timer.hide();
+            $(".palette-color").css("cursor", "")
+        }
+
+        if (this.cooldown === 0 && last !== 0) {
+            new Notification("Pxls.space", {
+                body: "Your next pixel is available!"
+            });
+        }
+    }
+};
+
+App.init();
---
+++ src/main/java/space/pxls/App.java
@@ -0,0 +1,196 @@
+package space.pxls;
+
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import spark.ResponseTransformer;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFiles;
+import static spark.Spark.webSocket;
+
+public class App {
+    private static int width = 1000;
+    private static int height = 1000;
+    private static byte[] board = new byte[width * height];
+    private static List<String> palette = Arrays.asList("#FFFFFF", "#E4E4E4", "#888888", "#222222", "#FFA7D1", "#E50000", "#E59500", "#A06A42", "#E5D900", "#94E044", "#02BE01", "#00D3DD", "#0083C7", "#0000EA", "#CF6EE4", "#820080");
+    private static int cooldown = 300;
+
+    public static void main(String[] args) {
+        try {
+            loadBoard();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        webSocket("/ws", new WSHandler());
+
+        staticFiles.location("/public");
+
+        get("/boardinfo", (req, res) -> {
+            res.type("application/json");
+            return new BoardInfo(width, height, palette);
+        }, new JsonTransformer());
+
+        get("/boarddata", (req, res) -> {
+            res.header("Content-Encoding", "gzip");
+            return board;
+        });
+
+    }
+
+    private static void loadBoard() throws IOException {
+        byte[] data = Files.readAllBytes(getBoardFile());
+        System.arraycopy(data, 0, board, 0, Math.min(data.length, board.length));
+    }
+
+    private static void saveBoard() throws IOException {
+        Files.write(getBoardFile(), board);
+    }
+
+    private static Path getBoardFile() {
+        return Paths.get(System.getProperty("boardFile", "board.dat"));
+    }
+
+    private static Path getLogFile() {
+        return Paths.get(System.getProperty("logFile", "pixels.log"));
+    }
+
+    @WebSocket
+    public static class WSHandler {
+        private Gson gson = new Gson();
+        private final Queue<Session> sessions = new ConcurrentLinkedQueue<>();
+
+        private ConcurrentHashMap<String, Long> lastPlaceTime = new ConcurrentHashMap<>();
+
+        @OnWebSocketConnect
+        public void connected(Session session) {
+            sessions.add(session);
+        }
+
+        @OnWebSocketClose
+        public void closed(Session session, int statusCode, String reason) {
+            sessions.remove(session);
+        }
+
+        @OnWebSocketMessage
+        public void message(Session session, String message) throws IOException {
+            session.getRemote().sendString(message);
+
+            PlaceRequest req = gson.fromJson(message, PlaceRequest.class);
+            int x = req.x;
+            int y = req.y;
+            int color = req.color;
+
+            if (x < 0 || x >= width || y < 0 || y >= width || color < 0 || color > palette.size()) return;
+
+            float waitTime = getWaitTime(session);
+            if (waitTime <= 0) {
+                lastPlaceTime.put(getIp(session), System.currentTimeMillis());
+                board[coordsToIndex(x, y)] = (byte) color;
+                for (Session loopSess : sessions) {
+                    loopSess.getRemote().sendString(gson.toJson(new BoardUpdate(x, y, color)));
+                }
+
+                saveBoard();
+                waitTime = cooldown;
+
+                log(session, x, y, color);
+            }
+
+            session.getRemote().sendString(gson.toJson(new WaitResponse((int) Math.floor(waitTime))));
+        }
+
+        private void log(Session session, int x, int y, int color) throws IOException {
+            FileWriter fw = new FileWriter(getLogFile().toFile(), true);
+            fw.append(String.valueOf(Instant.now().toEpochMilli())).append(" ").append(String.valueOf(x)).append(",").append(String.valueOf(y)).append(",").append(palette.get(color)).append(" by ").append(getIp(session)).append("\n");
+            fw.close();
+        }
+
+        private String getIp(Session sess) {
+            String ip = sess.getRemoteAddress().getHostName();
+            if (sess.getUpgradeRequest().getHeader("X-Forwarded-For") != null) {
+                ip = sess.getUpgradeRequest().getHeader("X-Forwarded-For");
+            }
+            return ip;
+        }
+
+        private float getWaitTime(Session sess) {
+            if (!lastPlaceTime.containsKey(getIp(sess))) return 0;
+
+            long lastPlace = lastPlaceTime.get(getIp(sess));
+            long nextPlace = lastPlace + cooldown * 1000;
+            return Math.max(0, nextPlace - System.currentTimeMillis());
+        }
+    }
+
+    private static int coordsToIndex(int x, int y) {
+        return y * width + x;
+    }
+
+    public static class PlaceRequest {
+        int x;
+        int y;
+        int color;
+    }
+
+    public static class WaitResponse {
+        String type = "cooldown";
+        int wait;
+
+        public WaitResponse(int wait) {
+            this.wait = wait;
+        }
+    }
+
+    public static class BoardUpdate {
+        String type = "pixel";
+        int x;
+        int y;
+        int color;
+
+        public BoardUpdate(int x, int y, int color) {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+        }
+    }
+
+    public static class BoardInfo {
+        int width;
+        int height;
+        List<String> palette;
+
+        public BoardInfo(int width, int height, List<String> palette) {
+            this.width = width;
+            this.height = height;
+            this.palette = palette;
+        }
+    }
+
+    public static class JsonTransformer implements ResponseTransformer {
+        private Gson gson = new Gson();
+
+        @Override
+        public String render(Object model) {
+            return gson.toJson(model);
+        }
+    }
+}
