6b1d443a5333e478f0ce7d3f90670c3c8b8790a5
Eden S.-G <eden@edensg.me>
2017-04-04 12:24:35 -0400

    Add timer in page title
    Resolves #6.

--- resources/public/app.js
+++ resources/public/app.js
@@ -306,6 +306,7 @@ window.App = {
             var minutes = Math.floor(this.cooldown / 60);
             var minuteStr = minutes < 10 ? "0" + minutes : minutes;
             this.elements.timer.text(minuteStr + ":" + secsStr);
+            document.title = minuteStr + ":" + secsStr + " - Pxls.space";
 
             $(".palette-color").css("cursor", "not-allowed")
         } else {
