4c7fc7ea8e998634379b2b7ef1a661982702289a
Ske <voltasalt@gmail.com>
2017-06-07 21:09:03 +0200

    A few more customizations

--- resources/public/pxls.js
+++ resources/public/pxls.js
@@ -1597,7 +1597,7 @@ window.App = (function () {
 
                         $(".palette-color").css("cursor", "not-allowed");
 
-                        document.title = "[" + minuteStr + ":" + secsStr + "] pxls.space";
+                        document.title = "[" + minuteStr + ":" + secsStr + "] pxls x vectorama";
 
                         if (self.runningTimer && !die) {
                             return;
@@ -1620,7 +1620,7 @@ window.App = (function () {
                         self.hasFiredNotification = true;
                     }
 
-                    document.title = "pxls.space";
+                    document.title = "pxls x vectorama";
                     self.elements.timer.hide();
                     $(".palette-color").css("cursor", "");
                 },
