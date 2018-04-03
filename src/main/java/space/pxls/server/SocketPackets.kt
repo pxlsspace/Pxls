40496779ec66b05ed73c7d9a040412ab0b467561
Leo Wandersleb <leo@leowandersleb.de>
2018-04-02 23:43:49 -0300

    bugfix: brought back removed "type"s
    lint falsly had claimed that these were never used but Gson sent them to
    the client. Suppressed the warning and brought them back

--- src/main/java/space/pxls/server/SocketPackets.kt
+++ src/main/java/space/pxls/server/SocketPackets.kt
@@ -1,5 +1,7 @@
-package space.pxls.server
+// unused `type` parameters are required and serialized when talking to the client using Gson.
+@file:Suppress("unused")
 
+package space.pxls.server
 data class ClientPlace(val type: String, val x: Int, val y: Int, val color: Int)
 data class ClientCaptcha(val token: String)
 data class ClientAdminCooldownOverride(val override: Boolean)
@@ -7,16 +9,27 @@ data class ClientAdminMessage(val username: String, val message: String)
 class ClientShadowBanMe
 class ClientBanMe
 class ClientUndo
-class ServerCaptchaRequired
-data class ServerCaptchaStatus(val success: Boolean)
+
+class ServerCaptchaRequired {
+    val type = "captcha_required"
+}
+
+data class ServerCaptchaStatus(val success: Boolean) {
+    val type = "captcha_status"
+}
 
 data class ServerPlace(val pixels: Collection<Pixel>) {
+    val type = "pixel"
     data class Pixel(val x: Int, val y: Int, val color: Int)
 }
 
-data class ServerCooldown(val wait: Float)
+data class ServerCooldown(val wait: Float) {
+    val type = "cooldown"
+}
 
-data class ServerUsers(val count: Int)
+data class ServerUsers(val count: Int) {
+    val type = "users"
+}
 
 data class ServerUserInfo(
         val username: String,
@@ -24,12 +37,22 @@ data class ServerUserInfo(
         val banned: Boolean,
         val banExpiry: Long,
         val ban_reason: String,
-        val method: String)
+        val method: String) {
+    val type = "userinfo"
+}
 
-data class ServerCanUndo(val time: Long)
+data class ServerCanUndo(val time: Long) {
+    val type = "can_undo"
+}
 
-data class ServerAlert(val message: String)
+data class ServerAlert(val message: String) {
+    val type = "alert"
+}
 
-data class ServerStack(val count: Int, val cause: String)
+data class ServerStack(val count: Int, val cause: String) {
+    val type = "stack"
+}
 
-data class ServerACK(val ackFor: String, val x: Int, val y: Int)
+data class ServerACK(val ackFor: String, val x: Int, val y: Int) {
+    val type="ACK"
+}
