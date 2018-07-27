47aafc1dae1d1c3dfbbb11d418a14650a4217fa3
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-07-27 19:42:15 -0400

    Flash the coords bubble teal when link is copied to clipboard

--- resources/public/pxls.js
+++ resources/public/pxls.js
@@ -2218,6 +2218,10 @@ window.App = (function () {
                     $(window).keydown(event => {
                         if ((event.key === "c" || event.key === "C" || event.keyCode === 67) && navigator.clipboard && self.mouseCoords) {
                             navigator.clipboard.writeText(location.origin + `/#x=${Math.floor(self.mouseCoords.x)}&y=${Math.floor(self.mouseCoords.y)}`);
+                            self.elements.coords.addClass("copyPulse");
+                            setTimeout(() => {
+                                self.elements.coords.removeClass("copyPulse");
+                            }, 200);
                         }
                     });
                 }
