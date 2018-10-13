c1710e17e6d7e32b8bc0f515a872725e240ee0ed
The_Rebel_G0D <rebel@trg0d.me>
2018-10-13 12:35:53 -0500

    Remove 'failed to play custom notification' dialog, instead dump error to console

--- resources/public/pxls.js
+++ resources/public/pxls.js
@@ -2209,7 +2209,7 @@ window.App = (function () {
                     }
 
                     timer.audioElem.addEventListener("error", err => {
-                        alert.show("Your custom alert is either invalid or unplayable.");
+                        if (console.warn) console.warn("An error occurred on the audioElem node: %o", err);
                     });
 
                     self.elements.txtAlertLocation.change(function() { //change should only fire on blur so we normally won't be calling updateAudio for each keystroke. just in case though, we'll lazy update.
