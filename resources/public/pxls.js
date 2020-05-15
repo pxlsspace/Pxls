af9b999d9491df90538c53b3e3f69d81fb23ccd7
BlankParenthesis <blank.parenthesis@gmail.com>
2020-05-15 18:46:28 +0100

    Expose user namespace (#340)
    This exposes the following functions from the internal user namespace:
    - getUsername
    - getRole
    - isLoggedIn
    - isStaff
    
    This allows userscripts to obtain this information in a clean way
    without needing to scrape the page.

--- resources/public/pxls.js
+++ resources/public/pxls.js
@@ -6684,6 +6684,12 @@ window.App = (function() {
     },
     chat,
     typeahead: chat.typeahead,
+    user: {
+      getUsername: user.getUsername,
+      getRole: user.getRole,
+      isLoggedIn: user.isLoggedIn,
+      isStaff: user.isStaff
+    },
     modal
   };
 })();
