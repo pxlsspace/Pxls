ccb8efdec323768b8fc02ae2db9e71f68260dbdc
CP <64031946+cp-codes2@users.noreply.github.com>
2020-07-07 14:00:57 -0600

    Update PacketHandler.java

--- src/main/java/space/pxls/server/PacketHandler.java
+++ src/main/java/space/pxls/server/PacketHandler.java
@@ -384,6 +384,8 @@ public class PacketHandler {
                     if (t < 0 && !user.hasPermission("chat.usercolor.rainbow")) {
                         server.send(channel, new ServerACKClientUpdate(false, "Color reserved for staff members", "NameColor", null));
                         return;
+                    if (t < 0 && !user.hasPermission("chat.usercolor.donator")) {
+                        server.send(channel, new ServerACKClientUpdate(false, "Color reserved for donators", "NameColor", null));
                     }
                     user.setChatNameColor(t, true);
                     server.send(channel, new ServerACKClientUpdate(true, null, "NameColor", String.valueOf(t)));
