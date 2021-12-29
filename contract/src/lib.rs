3240b8da139b2a3c8c9208a4f67c116c8fe2e975
singulart <serpian@gmail.com>
2021-12-29 16:14:49 +0200

    sharded structure gives Memory out of bounds trap on deploy

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -9,10 +9,29 @@ use crate::borsh::maybestd::{
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-const BOARD_SIDE: usize = 100;
+const BOARD_SIDE: usize = 1000;
+const SHARD_SIDE: usize = 250;
 const BOARD_DIMENSIONS: usize = BOARD_SIDE * BOARD_SIDE;
+const SHARD_DIMENSIONS: usize = SHARD_SIDE * SHARD_SIDE;
 
-pub struct Board([u8; BOARD_DIMENSIONS]);
+pub struct Board {
+    sh00: [u8; SHARD_DIMENSIONS],
+    sh01: [u8; SHARD_DIMENSIONS],
+    sh02: [u8; SHARD_DIMENSIONS],
+    sh03: [u8; SHARD_DIMENSIONS],
+    sh10: [u8; SHARD_DIMENSIONS],
+    sh11: [u8; SHARD_DIMENSIONS],
+    sh12: [u8; SHARD_DIMENSIONS],
+    sh13: [u8; SHARD_DIMENSIONS],
+    sh20: [u8; SHARD_DIMENSIONS],
+    sh21: [u8; SHARD_DIMENSIONS],
+    sh22: [u8; SHARD_DIMENSIONS],
+    sh23: [u8; SHARD_DIMENSIONS],
+    sh30: [u8; SHARD_DIMENSIONS],
+    sh31: [u8; SHARD_DIMENSIONS],
+    sh32: [u8; SHARD_DIMENSIONS],
+    sh33: [u8; SHARD_DIMENSIONS],
+}
 
 #[derive(BorshStorageKey, BorshSerialize)]
 pub enum StorageKeys {
@@ -29,7 +48,7 @@ impl BorshSerialize for Board {
     
     #[inline]
     fn serialize<W: Write>(&self, writer: &mut W) -> Result<()> {
-        let written: Result<usize> =  writer.write(&self.0);
+        let written: Result<usize> =  writer.write(&self.sh00);
         env::log(written.unwrap().to_string().as_bytes());
         Ok(())
         //self.0.serialize(writer)
@@ -40,10 +59,25 @@ impl BorshDeserialize for Board {
 
     #[inline]
     fn deserialize(buf: &mut &[u8]) -> Result<Self> {
-        let mut board = Board([0; BOARD_DIMENSIONS]);
-        for i in 0..BOARD_DIMENSIONS {
-            board.0[i] = buf[i]
-        }
+        let mut board = Board {
+            sh00: [0; SHARD_DIMENSIONS],
+            sh01: [0; SHARD_DIMENSIONS],
+            sh02: [0; SHARD_DIMENSIONS],
+            sh03: [0; SHARD_DIMENSIONS],
+            sh10: [0; SHARD_DIMENSIONS],
+            sh11: [0; SHARD_DIMENSIONS],
+            sh12: [0; SHARD_DIMENSIONS],
+            sh13: [0; SHARD_DIMENSIONS],
+            sh20: [0; SHARD_DIMENSIONS],
+            sh21: [0; SHARD_DIMENSIONS],
+            sh22: [0; SHARD_DIMENSIONS],
+            sh23: [0; SHARD_DIMENSIONS],
+            sh30: [0; SHARD_DIMENSIONS],
+            sh31: [0; SHARD_DIMENSIONS],
+            sh32: [0; SHARD_DIMENSIONS],
+            sh33: [0; SHARD_DIMENSIONS],
+        };
+        //board.0.copy_from_slice(buf);
         *buf = &[];
         Ok(board)
     }
@@ -55,14 +89,24 @@ impl NearCanvas {
     #[init]
     pub fn new() -> Self {
         Self {
-            board: Board([0; BOARD_DIMENSIONS])
+            board: Board {
+                sh00: [0; SHARD_DIMENSIONS],
+                sh01: [0; SHARD_DIMENSIONS],
+                sh02: [0; SHARD_DIMENSIONS],
+                sh03: [0; SHARD_DIMENSIONS],
+                sh10: [0; SHARD_DIMENSIONS],
+                sh11: [0; SHARD_DIMENSIONS],
+                sh12: [0; SHARD_DIMENSIONS],
+                sh13: [0; SHARD_DIMENSIONS],
+            }
         }
 
     }
 
     pub fn put_pixel(&mut self, x: usize, y: usize, color: u8) {
         // let applicant = env::signer_account_id();
-        self.board.0[x + y * BOARD_SIDE] = color
+        env::log(b"put_pixel!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        self.board.sh00[x + y * BOARD_SIDE] = color
     }
 
 }
