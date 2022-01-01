62e6bea58b91c950734aeb94f27823e70864720b
singulart <serpian@gmail.com>
2022-01-01 10:40:13 +0100

    lazy sharding still giving the Memory out of bounds trap on deploy

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,6 +1,6 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{env, near_bindgen, PanicOnDefault};
-use near_sdk::BorshStorageKey;
+use near_sdk::store::LazyOption;
 use crate::borsh::maybestd::{
     io::{Result, Write},
 };
@@ -11,77 +11,54 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 const BOARD_SIDE: usize = 1000;
 const SHARD_SIDE: usize = 250;
-const BOARD_DIMENSIONS: usize = BOARD_SIDE * BOARD_SIDE;
 const SHARD_DIMENSIONS: usize = SHARD_SIDE * SHARD_SIDE;
 
-pub struct Board {
-    sh00: [u8; SHARD_DIMENSIONS],
-    sh01: [u8; SHARD_DIMENSIONS],
-    sh02: [u8; SHARD_DIMENSIONS],
-    sh03: [u8; SHARD_DIMENSIONS],
-    sh10: [u8; SHARD_DIMENSIONS],
-    sh11: [u8; SHARD_DIMENSIONS],
-    sh12: [u8; SHARD_DIMENSIONS],
-    sh13: [u8; SHARD_DIMENSIONS],
-    sh20: [u8; SHARD_DIMENSIONS],
-    sh21: [u8; SHARD_DIMENSIONS],
-    sh22: [u8; SHARD_DIMENSIONS],
-    sh23: [u8; SHARD_DIMENSIONS],
-    sh30: [u8; SHARD_DIMENSIONS],
-    sh31: [u8; SHARD_DIMENSIONS],
-    sh32: [u8; SHARD_DIMENSIONS],
-    sh33: [u8; SHARD_DIMENSIONS],
+pub struct BoardArray([u8; SHARD_DIMENSIONS]);
+
+impl BorshSerialize for BoardArray {
+    
+    #[inline]
+    fn serialize<W: Write>(&self, writer: &mut W) -> Result<()> {
+        let written: Result<usize> =  writer.write(&self.0);
+        env::log(written.unwrap().to_string().as_bytes());
+        Ok(())
+    }
+}
+impl BorshDeserialize for BoardArray {
+
+    #[inline]
+    fn deserialize(buf: &mut &[u8]) -> Result<Self> {
+        let mut board = BoardArray([0; SHARD_DIMENSIONS]);
+        for i in 0..SHARD_DIMENSIONS {
+            board.0[i] = buf[i]
+        }
+        *buf = &[];
+        Ok(board)        
+    }
 }
 
-#[derive(BorshStorageKey, BorshSerialize)]
-pub enum StorageKeys {
-    Board,
-}
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct NearCanvas {
-    board: Board
+    sh00: LazyOption<BoardArray>,
+    sh01: LazyOption<BoardArray>,
+    sh02: LazyOption<BoardArray>,
+    sh03: LazyOption<BoardArray>,
+    sh10: LazyOption<BoardArray>,
+    sh11: LazyOption<BoardArray>,
+    sh12: LazyOption<BoardArray>,
+    sh13: LazyOption<BoardArray>,
+    sh20: LazyOption<BoardArray>,
+    sh21: LazyOption<BoardArray>,
+    sh22: LazyOption<BoardArray>,
+    sh23: LazyOption<BoardArray>,
+    sh30: LazyOption<BoardArray>,
+    sh31: LazyOption<BoardArray>,
+    sh32: LazyOption<BoardArray>,
+    sh33: LazyOption<BoardArray>,
 }
 
-impl BorshSerialize for Board {
-    
-    #[inline]
-    fn serialize<W: Write>(&self, writer: &mut W) -> Result<()> {
-        let written: Result<usize> =  writer.write(&self.sh00);
-        env::log(written.unwrap().to_string().as_bytes());
-        Ok(())
-        //self.0.serialize(writer)
-    }
-}
-
-impl BorshDeserialize for Board {
-
-    #[inline]
-    fn deserialize(buf: &mut &[u8]) -> Result<Self> {
-        let mut board = Board {
-            sh00: [0; SHARD_DIMENSIONS],
-            sh01: [0; SHARD_DIMENSIONS],
-            sh02: [0; SHARD_DIMENSIONS],
-            sh03: [0; SHARD_DIMENSIONS],
-            sh10: [0; SHARD_DIMENSIONS],
-            sh11: [0; SHARD_DIMENSIONS],
-            sh12: [0; SHARD_DIMENSIONS],
-            sh13: [0; SHARD_DIMENSIONS],
-            sh20: [0; SHARD_DIMENSIONS],
-            sh21: [0; SHARD_DIMENSIONS],
-            sh22: [0; SHARD_DIMENSIONS],
-            sh23: [0; SHARD_DIMENSIONS],
-            sh30: [0; SHARD_DIMENSIONS],
-            sh31: [0; SHARD_DIMENSIONS],
-            sh32: [0; SHARD_DIMENSIONS],
-            sh33: [0; SHARD_DIMENSIONS],
-        };
-        //board.0.copy_from_slice(buf);
-        *buf = &[];
-        Ok(board)
-    }
-}
 
 #[near_bindgen]
 impl NearCanvas {
@@ -89,24 +66,153 @@ impl NearCanvas {
     #[init]
     pub fn new() -> Self {
         Self {
-            board: Board {
-                sh00: [0; SHARD_DIMENSIONS],
-                sh01: [0; SHARD_DIMENSIONS],
-                sh02: [0; SHARD_DIMENSIONS],
-                sh03: [0; SHARD_DIMENSIONS],
-                sh10: [0; SHARD_DIMENSIONS],
-                sh11: [0; SHARD_DIMENSIONS],
-                sh12: [0; SHARD_DIMENSIONS],
-                sh13: [0; SHARD_DIMENSIONS],
-            }
+            sh00: LazyOption::new(b"a", Some(BoardArray([0; SHARD_DIMENSIONS]))),
+            sh01: LazyOption::new(b"b", Some(BoardArray([0; SHARD_DIMENSIONS]))),
+            sh02: LazyOption::new(b"c", Some(BoardArray([0; SHARD_DIMENSIONS]))),
+            sh03: LazyOption::new(b"d", Some(BoardArray([0; SHARD_DIMENSIONS]))),
+            sh10: LazyOption::new(b"e", Some(BoardArray([0; SHARD_DIMENSIONS]))),
+            sh11: LazyOption::new(b"f", Some(BoardArray([0; SHARD_DIMENSIONS]))),
+            sh12: LazyOption::new(b"g", Some(BoardArray([0; SHARD_DIMENSIONS]))),
+            sh13: LazyOption::new(b"h", Some(BoardArray([0; SHARD_DIMENSIONS]))),
+            sh20: LazyOption::new(b"i", Some(BoardArray([0; SHARD_DIMENSIONS]))),
+            sh21: LazyOption::new(b"j", Some(BoardArray([0; SHARD_DIMENSIONS]))),
+            sh22: LazyOption::new(b"k", Some(BoardArray([0; SHARD_DIMENSIONS]))),
+            sh23: LazyOption::new(b"l", Some(BoardArray([0; SHARD_DIMENSIONS]))),
+            sh30: LazyOption::new(b"m", Some(BoardArray([0; SHARD_DIMENSIONS]))),
+            sh31: LazyOption::new(b"n", Some(BoardArray([0; SHARD_DIMENSIONS]))),
+            sh32: LazyOption::new(b"o", Some(BoardArray([0; SHARD_DIMENSIONS]))),
+            sh33: LazyOption::new(b"p", Some(BoardArray([0; SHARD_DIMENSIONS]))),
         }
 
     }
 
     pub fn put_pixel(&mut self, x: usize, y: usize, color: u8) {
-        // let applicant = env::signer_account_id();
-        env::log(b"put_pixel!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        self.board.sh00[x + y * BOARD_SIDE] = color
+        if x >= BOARD_SIDE || y >= BOARD_SIDE {
+            env::log_str("Illegal coordinates");
+        } else {
+            let shard = self.get_mutable_shard(x, y);
+            let mut shard_data: [u8; SHARD_DIMENSIONS] = shard.get().as_ref().unwrap().0;
+            shard_data[ x % SHARD_SIDE + (y % SHARD_SIDE) * SHARD_SIDE] = color;
+            shard.flush()
+
+            // self.sh00.get_mut().as_ref().unwrap().0[x % SHARD_SIDE + (y % SHARD_SIDE) * SHARD_SIDE] = color;
+            // self.sh00.flush();
+        }
+    }
+
+    pub fn get_pixel(& self, x: usize, y: usize) -> u8 {
+        if x >= BOARD_SIDE || y >= BOARD_SIDE {
+            env::log(b"Illegal coordinates");
+            0
+        } else {
+            let shard : &LazyOption<BoardArray> = self.get_shard(x, y);
+            let shard_data: [u8; SHARD_DIMENSIONS] = shard.get().as_ref().unwrap().0;
+            shard_data[ x % SHARD_SIDE + (y % SHARD_SIDE) * SHARD_SIDE]
+        }
+    }
+
+    fn get_shard(&self, x: usize, y: usize) -> &LazyOption<BoardArray> {
+        if x / SHARD_SIDE == 0 && y / SHARD_SIDE == 0 {
+            env::log(format!("shard{}{}", 0, 0).as_bytes());
+            &self.sh00
+        } else if x / SHARD_SIDE == 0 && y / SHARD_SIDE == 1 {
+            env::log(format!("shard{}{}", 0, 1).as_bytes());
+            &self.sh01
+        } else if x / SHARD_SIDE == 0 && y / SHARD_SIDE == 2 {
+            env::log(format!("shard{}{}", 0, 2).as_bytes());
+            &self.sh02
+        } else if x / SHARD_SIDE == 0 && y / SHARD_SIDE == 3 {
+            env::log(format!("shard{}{}", 0, 3).as_bytes());
+            &self.sh03
+        } else if x / SHARD_SIDE == 1 && y / SHARD_SIDE == 0 {
+            env::log(format!("shard{}{}", 1, 0).as_bytes());
+            &self.sh10
+        } else if x / SHARD_SIDE == 1 && y / SHARD_SIDE == 1 {
+            env::log(format!("shard{}{}", 1, 1).as_bytes());
+            &self.sh11
+        } else if x / SHARD_SIDE == 1 && y / SHARD_SIDE == 2 {
+            env::log(format!("shard{}{}", 1, 2).as_bytes());
+            &self.sh12
+        } else if x / SHARD_SIDE == 1 && y / SHARD_SIDE == 3 {
+            env::log(format!("shard{}{}", 1, 3).as_bytes());
+            &self.sh13
+        } else if x / SHARD_SIDE == 2 && y / SHARD_SIDE == 0 {
+            env::log(format!("shard{}{}", 2, 0).as_bytes());
+            &self.sh20
+        } else if x / SHARD_SIDE == 2 && y / SHARD_SIDE == 1 {
+            env::log(format!("shard{}{}", 2, 1).as_bytes());
+            &self.sh21
+        } else if x / SHARD_SIDE == 2 && y / SHARD_SIDE == 2 {
+            env::log(format!("shard{}{}", 2, 2).as_bytes());
+            &self.sh22
+        } else if x / SHARD_SIDE == 2 && y / SHARD_SIDE == 3 {
+            env::log(format!("shard{}{}", 2, 3).as_bytes());
+            &self.sh23
+        } else if x / SHARD_SIDE == 3 && y / SHARD_SIDE == 0 {
+            env::log(format!("shard{}{}", 3, 0).as_bytes());
+            &self.sh30
+        } else if x / SHARD_SIDE == 3 && y / SHARD_SIDE == 1 {
+            env::log(format!("shard{}{}", 3, 1).as_bytes());
+            &self.sh31
+        } else if x / SHARD_SIDE == 3 && y / SHARD_SIDE == 2 {
+            env::log(format!("shard{}{}", 3, 2).as_bytes());
+            &self.sh32
+        } else {
+            env::log(format!("shard{}{}", 3, 3).as_bytes());
+            &self.sh33
+        } 
+    }
+
+    fn get_mutable_shard(&mut self, x: usize, y: usize) -> &mut LazyOption<BoardArray> {
+        if x / SHARD_SIDE == 0 && y / SHARD_SIDE == 0 {
+            env::log(format!("shard{}{}", 0, 0).as_bytes());
+            &mut self.sh00
+        } else if x / SHARD_SIDE == 0 && y / SHARD_SIDE == 1 {
+            env::log(format!("shard{}{}", 0, 1).as_bytes());
+            &mut self.sh01
+        } else if x / SHARD_SIDE == 0 && y / SHARD_SIDE == 2 {
+            env::log(format!("shard{}{}", 0, 2).as_bytes());
+            &mut self.sh02
+        } else if x / SHARD_SIDE == 0 && y / SHARD_SIDE == 3 {
+            env::log(format!("shard{}{}", 0, 3).as_bytes());
+            &mut self.sh03
+        } else if x / SHARD_SIDE == 1 && y / SHARD_SIDE == 0 {
+            env::log(format!("shard{}{}", 1, 0).as_bytes());
+            &mut self.sh10
+        } else if x / SHARD_SIDE == 1 && y / SHARD_SIDE == 1 {
+            env::log(format!("shard{}{}", 1, 1).as_bytes());
+            &mut self.sh11
+        } else if x / SHARD_SIDE == 1 && y / SHARD_SIDE == 2 {
+            env::log(format!("shard{}{}", 1, 2).as_bytes());
+            &mut self.sh12
+        } else if x / SHARD_SIDE == 1 && y / SHARD_SIDE == 3 {
+            env::log(format!("shard{}{}", 1, 3).as_bytes());
+            &mut self.sh13
+        } else if x / SHARD_SIDE == 2 && y / SHARD_SIDE == 0 {
+            env::log(format!("shard{}{}", 2, 0).as_bytes());
+            &mut self.sh20
+        } else if x / SHARD_SIDE == 2 && y / SHARD_SIDE == 1 {
+            env::log(format!("shard{}{}", 2, 1).as_bytes());
+            &mut self.sh21
+        } else if x / SHARD_SIDE == 2 && y / SHARD_SIDE == 2 {
+            env::log(format!("shard{}{}", 2, 2).as_bytes());
+            &mut self.sh22
+        } else if x / SHARD_SIDE == 2 && y / SHARD_SIDE == 3 {
+            env::log(format!("shard{}{}", 2, 3).as_bytes());
+            &mut self.sh23
+        } else if x / SHARD_SIDE == 3 && y / SHARD_SIDE == 0 {
+            env::log(format!("shard{}{}", 3, 0).as_bytes());
+            &mut self.sh30
+        } else if x / SHARD_SIDE == 3 && y / SHARD_SIDE == 1 {
+            env::log(format!("shard{}{}", 3, 1).as_bytes());
+            &mut self.sh31
+        } else if x / SHARD_SIDE == 3 && y / SHARD_SIDE == 2 {
+            env::log(format!("shard{}{}", 3, 2).as_bytes());
+            &mut self.sh32
+        } else {
+            env::log(format!("shard{}{}", 3, 3).as_bytes());
+            &mut self.sh33
+        } 
     }
 
 }
