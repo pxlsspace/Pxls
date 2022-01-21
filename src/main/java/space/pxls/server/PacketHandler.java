157bca07f188d2bbd0bfe7bf9427d42e9ab321d0
BlankParenthesis <blank.parenthesis@gmail.com>
2022-01-21 19:23:14 +0000

    Add configurable chat echo via webhooks (#511)
    * Add configurable chat echo via webhooks
    
    This probably only supports Discord's webhooks.
    
    * Add pixel count and faction tag to chat webhook
    
    * Add message ID to chat webhook footer
    
    * Add note to pixel count badge in chat webhook
    
    * Break markdown links in chat webhook
    
    * Use var instead of String in chat webhook
    
    Co-authored-by: Vanilla Black <vanilla@f66.dev>

--- src/main/java/space/pxls/server/PacketHandler.java
+++ src/main/java/space/pxls/server/PacketHandler.java
@@ -8,6 +8,9 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import com.typesafe.config.Config;
 import io.undertow.websockets.core.WebSocketChannel;
 import org.jdbi.v3.core.statement.UnableToExecuteStatementException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import space.pxls.App;
 import space.pxls.data.DBChatMessage;
 import space.pxls.data.DBPixelPlacementFull;
@@ -17,6 +20,11 @@ import space.pxls.user.Faction;
 import space.pxls.user.User;
 import space.pxls.util.TextFilter;
 import space.pxls.util.RateLimitFactory;
+
+import java.io.*;
+import java.net.*;
+
+import java.time.Instant;
 
 import java.util.*;
 import java.util.concurrent.TimeUnit;
@@ -481,10 +489,87 @@ public class PacketHandler {
                 }
                 if (userPacket != null || staffPacket != null) {
                     server.broadcastSeparateForStaff(userPacket, staffPacket);
+                    if(userPacket != null) {
+                        relayChatMessageToWebhooks(userPacket.getMessage(), App.getConfig().getStringList("chat.publicWebhooks"));
+                    }
+
+                    if(staffPacket != null) {
+                        relayChatMessageToWebhooks(staffPacket.getMessage(), App.getConfig().getStringList("chat.staffWebhooks"));
+                    }
                 }
             } catch (UnableToExecuteStatementException utese) {
                 utese.printStackTrace();
                 System.err.println("Failed to execute the ChatMessage insert statement.");
+            }
+        }
+    }
+
+    private void relayChatMessageToWebhooks(ChatMessage message, List<String> webhooks) {
+        // NOTE ([  ]): these are very much discord embeds at the moment.
+        // see https://discord.com/developers/docs/resources/channel#embed-object
+        var embed = new JSONObject();
+
+        var description = message.getMessage_raw()
+                // NOTE (Flying): This suffices for breaking markdown links.
+                .replaceAll("\\[", "\\\\[");
+
+        embed.put("description", description);
+        embed.put("timestamp", Instant.ofEpochSecond(message.getDate()).toString());
+        embed.put("color", Long.decode("0x" + App.getPalette().getColors().get(message.getAuthorNameColor().intValue()).getValue()));
+
+        var author = new JSONObject();
+        // NOTE ([  ]): There's no clean way to determining if we're on http or https
+        // so I gave up — you should be using https anyway.
+        try {
+            var authorProfile = new URL("https://" + App.getConfig().getString("host") + "/profile/" + message.getAuthor() + "/");
+
+            // NOTE (Flying): The pixel count badge seems to always come last.
+            var pixelCount = message.getBadges().get(message.getBadges().size() - 1).getDisplayName() + " ";
+            var factionTag = message.getStrippedFaction() != null ? "[" + message.getStrippedFaction().getTag() + "] " : "";
+            author.put("name", pixelCount + factionTag + message.getAuthor());
+            author.put("url", authorProfile);
+
+            embed.put("author", author);
+        } catch(MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        var footer = new JSONObject();
+
+        footer.put("text", message.getId());
+
+        embed.put("footer", footer);
+
+        var postDataBuilder = new JSONObject();
+
+        postDataBuilder.put("embeds", new JSONObject[] { embed });
+
+        var postData = postDataBuilder.toString();
+
+        for(var hook : webhooks) {
+            try {
+                var connection = (HttpURLConnection) new URL(hook).openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setDoOutput(true);
+                var postDataStream = new DataOutputStream(connection.getOutputStream());
+                postDataStream.writeBytes(postData);
+                postDataStream.flush();
+                postDataStream.close();
+
+                // NOTE ([  ]): this error code might be a bit cryptic when printed,
+                // but I don't want to clean it up and it's better than failing silently.
+                if(connection.getResponseCode() >= 400) {
+                    var response = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+
+                    for(var line: response.lines().collect(Collectors.toList())) {
+                        System.err.println(line);
+                    }
+
+                    response.close();
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
             }
         }
     }
