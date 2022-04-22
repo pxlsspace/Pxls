79ee15a257412800d5f926653ab2a8d17e9c0b80
Vanilla Black <vanilla@f66.dev>
2022-04-22 12:28:57 -0400

    Remove extraneous single quote

--- resources/public/include/lookup.js
+++ resources/public/include/lookup.js
@@ -30,7 +30,7 @@ module.exports.lookup = (function() {
           }
           msg = textarea;
         } else if (textarea !== '') {
-          msg += `; ${__('additional information:')} ' ${textarea}`;
+          msg += `; ${__('additional information:')} ${textarea}`;
         }
         $.post('/report', {
           id: id,
