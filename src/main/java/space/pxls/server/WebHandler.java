pxlsspace/Pxls

commit 91be30c0e3fd7b77d4b35443d0c818350bb9af47

Fix regex for discord name update (#672)
Fixes #675

* Fix regex for discord name update

* Fix regex for Discord username on sign in

* Change the format of discord name update placeholder

* Update the placeholder for signup

src/main/java/space/pxls/server/WebHandler.java
@@ -1351,11 +1351,11 @@ public class WebHandler {
 
         if (discordName != null) {
             if (discordName.contains("#") && !discordName.matches("^.{2,32}#\\d{4}$")){
-                sendBadRequest(exchange, "name isn't in the format '{name}#{discriminator}'");
+                sendBadRequest(exchange, "Name isn't in the format '{name}#{discriminator}'");
                 return;
             }
-            if (!discordName.contains("#") && !discordName.matches("^[A-Za-z0-9._]{2,32}$")){
-                sendBadRequest(exchange, "name isn't in the discord tag format (only english letters, numbers, periods and underlines allowed)");
+            if (!discordName.contains("#") && !discordName.matches("^[a-z0-9._]{2,32}$")){
+                sendBadRequest(exchange, "Name isn't in the discord tag format (only lowercase english letters, digits, periods and underlines allowed)");
                 return;
             }
         }
@@ -1670,9 +1670,15 @@ public class WebHandler {
         } else if (!name.matches("[a-zA-Z0-9_\\-]+")) {
             respond(exchange, StatusCodes.BAD_REQUEST, new space.pxls.server.packets.http.Error("bad_username", "Username contains invalid characters"));
             return;
-        } else if (!discord.isEmpty() && !discord.matches("^.+?#\\d{4}")) {
-            respond(exchange, StatusCodes.BAD_REQUEST, new space.pxls.server.packets.http.Error("bad_discord", "Discord tag contains invalid characters"));
-            return;
+        } else if (!discord.isEmpty()) {
+            if (discord.contains("#") && !discord.matches("^.{2,32}#\\d{4}$")){
+                respond(exchange, StatusCodes.BAD_REQUEST, new space.pxls.server.packets.http.Error("bad_discord", "Discord name isn't in the format '{name}#{discriminator}'"));
+                return;
+            }
+            if (!discord.contains("#") && !discord.matches("^[a-z0-9._]{2,32}$")){
+                respond(exchange, StatusCodes.BAD_REQUEST, new space.pxls.server.packets.http.Error("bad_discord", "Discord name isn't in the discord tag format (only lowercase english letters, digits, periods and underlines allowed)"));
+                return;
+            }
         } else if (!App.getUserManager().isValidSignupToken(token)) {
             respond(exchange, StatusCodes.BAD_REQUEST, new space.pxls.server.packets.http.Error("bad_token", "Invalid signup token"));
             return;

resources/public/pebble_templates/index.html
@@ -144,7 +144,7 @@
             <p>
                 <label class="input-group">
                     <span class="label-text">{{i18n('Localization', 'Discord Tag (Optional):') | raw}}</span>
-                    <input id="signup-discord-input" type="text" maxlength="64" placeholder="Username#1234">
+                    <input id="signup-discord-input" type="text" maxlength="64" placeholder="username1234">
                 </label>
             </p>
           <div class="error" id="error"></div>
@@ -824,7 +824,7 @@
                     <div data-keywords="discord username">
                         <label class="input-group">
                             <span class="label-text">{{i18n('Localization', 'Public Discord name:') | raw}}</span>
-                            <input class="fullwidth" type="text" placeholder="pxlslover#1337" id="txtDiscordName">
+                            <input class="fullwidth" type="text" placeholder="pxlslover1337" id="txtDiscordName">
                         </label>
                         <div class="button-bar">
                             <button class="text-button" id="btnDiscordNameSet">{{i18n('Localization', 'Set') | raw}}</button>

commit 79d54fcac691dc5ce1ddf3396671414917de75d8

Fix signup working whenever a discord name is passed to the endpoint
This bug was introduced in #672 and Caught by @Lordshinjo

src/main/java/space/pxls/server/WebHandler.java
@@ -1670,7 +1670,12 @@ public class WebHandler {
         } else if (!name.matches("[a-zA-Z0-9_\\-]+")) {
             respond(exchange, StatusCodes.BAD_REQUEST, new space.pxls.server.packets.http.Error("bad_username", "Username contains invalid characters"));
             return;
-        } else if (!discord.isEmpty()) {
+        } else  if (!App.getUserManager().isValidSignupToken(token)) {
+            respond(exchange, StatusCodes.BAD_REQUEST, new space.pxls.server.packets.http.Error("bad_token", "Invalid signup token"));
+            return;
+        }
+
+        if (!discord.isEmpty()) {
             if (discord.contains("#") && !discord.matches("^.{2,32}#\\d{4}$")){
                 respond(exchange, StatusCodes.BAD_REQUEST, new space.pxls.server.packets.http.Error("bad_discord", "Discord name isn't in the format '{name}#{discriminator}'"));
                 return;
@@ -1679,9 +1684,6 @@ public class WebHandler {
                 respond(exchange, StatusCodes.BAD_REQUEST, new space.pxls.server.packets.http.Error("bad_discord", "Discord name isn't in the discord tag format (only lowercase english letters, digits, periods and underlines allowed)"));
                 return;
             }
-        } else if (!App.getUserManager().isValidSignupToken(token)) {
-            respond(exchange, StatusCodes.BAD_REQUEST, new space.pxls.server.packets.http.Error("bad_token", "Invalid signup token"));
-            return;
         }
 
         String ip = exchange.getAttachment(IPReader.IP);