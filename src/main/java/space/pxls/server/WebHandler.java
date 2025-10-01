db83d93a799ea00069fc127d6fcde82395ac2399
[  ] <blank.parenthesis@gmail.com>
2025-10-01 13:59:53 +0100

    Add new chat name gradients

--- src/main/java/space/pxls/server/WebHandler.java
+++ src/main/java/space/pxls/server/WebHandler.java
@@ -1027,7 +1027,7 @@ public class WebHandler {
 
         try {
             int t = Integer.parseInt(nameColor.getValue());
-            if (t >= -16 && t < App.getPalette().getColors().size()) {
+            if (t >= -19 && t < App.getPalette().getColors().size()) {
                 var hasAllDonatorColors = user.hasPermission("chat.usercolor.donator") || user.hasPermission("chat.usercolor.donator.*");
                 if (t == -1 && !user.hasPermission("chat.usercolor.rainbow")) {
                     sendBadRequest(exchange, "Color reserved for staff members");
@@ -1075,6 +1075,15 @@ public class WebHandler {
                     sendBadRequest(exchange, "Color reserved for donators");
                     return;
                 } else if (t == -16 && !(hasAllDonatorColors || user.hasPermission("chat.usercolor.donator.forest"))) {
+                    sendBadRequest(exchange, "Color reversed for donators");
+                    return;
+                } else if (t == -17 && !(hasAllDonatorColors || user.hasPermission("chat.usercolor.donator.purple"))) {
+                    sendBadRequest(exchange, "Color reversed for donators");
+                    return;
+                } else if (t == -18 && !(hasAllDonatorColors || user.hasPermission("chat.usercolor.donator.gay"))) {
+                    sendBadRequest(exchange, "Color reversed for donators");
+                    return;
+                } else if (t == -19 && !(hasAllDonatorColors || user.hasPermission("chat.usercolor.donator.lesbian"))) {
                     sendBadRequest(exchange, "Color reversed for donators");
                     return;
                 }
